0b5111e4ae454fa8a7bf21a6856513e61741ae05
tfilushandy <127372146+tfilushandy@users.noreply.github.com>
2023-06-04 15:39:19 +0700

    Add files via upload

---
+++ TUGAS DR HALIM/TUGAS DR HALIM/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TUGAS_DR_HALIM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.resulttxtbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(122, 86);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "button1";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // resulttxtbox
+            // 
+            this.resulttxtbox.Location = new System.Drawing.Point(203, 88);
+            this.resulttxtbox.Name = "resulttxtbox";
+            this.resulttxtbox.Size = new System.Drawing.Size(503, 20);
+            this.resulttxtbox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resulttxtbox);
+            this.Controls.Add(this.btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox resulttxtbox;
+    }
+}
+
---
+++ TUGAS DR HALIM/TUGAS DR HALIM/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TUGAS_DR_HALIM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string classname = "Knight";
+        static int VIT = 2;
+        static int STR = 0;
+        static int DEX = 2;
+        static int AGI = 0;
+        static int INT = 1;
+        static int WIS = 2;
+        static int CASH = 1000;
+        static int LEVEL = 2;
+        static int SP = 1;
+        static double POS = 200.69;
+        static string MAP = "12345234523456345623456456745673456734567345674567";
+
+        string result = $"{classname} {VIT.ToString()} {STR.ToString()} {DEX.ToString()} {AGI.ToString()}" +
+            $" {INT.ToString()} {WIS.ToString()} {CASH.ToString()} {LEVEL.ToString()} {SP.ToString()} {POS.ToString()} {MAP}";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*  Class Name 	: Knight
+                VIT		: 2
+                STR		: 0
+                DEX		: 2
+                AGI		: 0
+                INT		: 1
+                WIS		: 2
+                CASH		: 1000
+                LEVEL		: 2
+                SP		: 1
+                POS		: 200,69
+                MAP		: 12345234523456345623456456745673456734567345674567
+
+                result
+                Knight 2 0 2 0 1 2 1000 2 1 200,69 123
+            */
+        }
+        class Knight
+        {
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            resulttxtbox.Text = result;
+        }
+    }
+}
